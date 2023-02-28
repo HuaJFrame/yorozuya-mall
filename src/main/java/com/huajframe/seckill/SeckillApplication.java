cfb346a66bf83999ddc7bfee974b8d173ddc73fb
JFrame <2498936201@qq.com>
2023-02-28 18:18:12 +0800

    feat: 项目的创建

---
+++ src/main/java/com/huajframe/seckill/SeckillApplication.java
@@ -0,0 +1,18 @@
+package com.huajframe.seckill;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hua JFarmer
+ */
+@SpringBootApplication
+@MapperScan("com.huajframe.seckill.dao")
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huajframe/seckill/SellkillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huajframe.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellkillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
